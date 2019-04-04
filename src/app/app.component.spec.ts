e255a4072c87b59835dd076d776af3e02d846606
Alex <alex.thepaut33@gmail.com>
2019-04-04 17:40:43 +0200

    Begin of welcome page
    Class container globale

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,5 @@
-import {async, TestBed} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import {APP_CONFIG} from './app.config';
-import {environment} from '../environments/environment';
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,28 +7,25 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      imports: [
-        HttpClientModule,
-      ],
-      providers: [
-        {provide: APP_CONFIG, useValue: environment},
-      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'private-showcase'`, async(() => {
+  });
+
+  it(`should have as title 'github-search'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('private-showcase');
-  }));
-  it('should render title in a h1 tag', async(() => {
+    expect(app.title).toEqual('github-search');
+  });
+
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to private-showcase!');
-  }));
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to github-search!');
+  });
 });
