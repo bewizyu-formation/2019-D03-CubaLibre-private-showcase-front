fed89bbb44582ded7ddacbf0f070cf473f9b2116
Alex <alex.thepaut33@gmail.com>
2019-04-05 17:31:14 +0200

    Début authentification

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PATH_WELCOME, PATH_HOME } from '../app.routes.constantes';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private fb: FormBuilder) {
+  constructor(private router: Router, private fb: FormBuilder, private httpClient: HttpClient) {
     this.loginCtrl = fb.control('', [Validators.required]);
     this.passwordCtrl = fb.control('', [Validators.required]);
 
@@ -29,7 +30,15 @@ export class LoginComponent implements OnInit {
   }
 
   validate() {
-    this.router.navigate([PATH_HOME]);
+    this.httpClient.post(
+      'localhost:8080/login',
+      {
+        'username': this.userForm.value.login,
+        'password': this.userForm.value.password
+      }
+    ).subscribe(resp => {
+      this.router.navigate([PATH_HOME]);
+    });
   }
 
   toHome() {
