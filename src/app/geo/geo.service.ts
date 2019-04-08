62e76dfe10340660bdd2ca57f003bc4b659ecb69
Alex <alex.thepaut33@gmail.com>
2019-04-08 15:49:04 +0200

    Geo service fix de l'url, et fix bug autocomp

--- src/app/geo/geo.service.ts
+++ src/app/geo/geo.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { EnvironmentService } from '../services/environment.service';
 
-const PATH_BACK = 'http://localhost:8080';
-const PATH_COMMUNES = 'communes/';
-const PATH_DEPARTEMENTS = 'departements/';
+const PATH_COMMUNES = '/communes';
+const PATH_DEPARTEMENTS = '/departements';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeoService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private env: EnvironmentService) { }
 
   getCommunes(cityToFind: string): Observable<any> {
     const params = new HttpParams().set('nom', cityToFind);
-    return this.httpClient.get(`${PATH_BACK}/${PATH_COMMUNES}`, { params: params });
+    return this.httpClient.get(`${this.env.getPrivateShowcaseApiConfig().uri}${PATH_COMMUNES}/`, { params: params });
   }
 
   getDepartements(departementsToFind: string) {
     const params = new HttpParams().set('nom', departementsToFind);
-    return this.httpClient.get(`${PATH_BACK}/${PATH_DEPARTEMENTS}`, { params: params });
+    return this.httpClient.get(`${this.env.getPrivateShowcaseApiConfig().uri}${PATH_DEPARTEMENTS}/`, { params: params });
   }
 }
