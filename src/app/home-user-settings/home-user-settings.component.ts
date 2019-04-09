b3070d89b451bcc2ae57d7885125ec67b1c21b32
Alex <alex.thepaut33@gmail.com>
2019-04-09 15:02:19 +0200

    Affichage du bouton artiste

--- src/app/home-user-settings/home-user-settings.component.ts
+++ src/app/home-user-settings/home-user-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home-user-settings',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeUserSettingsComponent implements OnInit {
 
-  constructor() { }
+  isArtist: boolean;
 
-  ngOnInit() { }
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+  }
 
   navigateToProfil() {
     // To do
