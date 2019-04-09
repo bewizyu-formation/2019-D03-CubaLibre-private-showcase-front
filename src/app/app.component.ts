1af3a89585d85a302cb37d96f3d7784e24e25c27
Alex <alex.thepaut33@gmail.com>
2019-04-09 16:46:14 +0200

    Lint fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
-import {UserService} from './user/user.service';
-import {HelloRepository} from './hello/hello.repository';
-import {HttpErrorResponse} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { UserService } from './user/user.service';
+import { HelloRepository } from './hello/hello.repository';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,7 @@ export class AppComponent {
 
 
   constructor(private userService: UserService) {
-    if(localStorage.getItem('token') && userService.token === undefined){
+    if (localStorage.getItem('token') && userService.token === undefined) {
       userService.token = localStorage.getItem('token');
     }
   }
