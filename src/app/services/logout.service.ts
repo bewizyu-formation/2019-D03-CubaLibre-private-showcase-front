9e3c177c38711cacc821cff53a6d0f8124e10e64
Dorian <dorian.bonnamy@gmail.com>
2019-04-09 17:21:55 +0200

    TGHJKLOIUHB

---
+++ src/app/services/logout.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EnvironmentService } from './environment.service';
+import { PATH_WELCOME, PATH_LOGOUT } from '../app.routes.constantes';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoutService {
+
+  constructor(private httpClient: HttpClient, private env: EnvironmentService, private router: Router) { }
+
+  setDisconnect() {
+
+    this.httpClient.get(`${this.env.getPrivateShowcaseApiConfig().uri}/${PATH_LOGOUT}/`)
+      .subscribe(() => {
+        this.router.navigate([PATH_WELCOME]);
+      });
+  }
+}
